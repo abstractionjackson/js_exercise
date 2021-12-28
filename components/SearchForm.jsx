6704d801a718eb3cb7e37b6e7c0509d77308a899
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2021-12-28 08:49:44 -0800

    add pagination page number nav

--- components/SearchForm.jsx
+++ components/SearchForm.jsx
@@ -11,7 +11,7 @@ export default function SearchForm({ handleSearch }) {
 
     return (
     <form className="p-3" onSubmit={(e) => handleSubmit(e, name)}>
-        <input className="mr-3 p-1 shadow focus:outline-none focus:ring focus:ring-pink-300" type="text" id="name" onChange={handleChange} placeholder="Find a charity..." value={name} />
+        <input required className="mr-3 p-1 shadow focus:outline-none focus:ring focus:ring-pink-300" type="text" id="name" onChange={handleChange} placeholder="Find a charity..." value={name} />
         <button className="p-1 bg-gray-200 hover:border hover:border-gray-300  focus:outline-none focus:border focus:border-gray-300" type="submit">Search</button>
      </form>
     )
--- components/SearchResultNav.jsx
+++ components/SearchResultNav.jsx
@@ -7,12 +7,57 @@ export default function SearchResultNav({
     },
     handlePageChange
 }) {
+    const CHARITIES_PER_PAGE = 10;
+
+    const getNumPages = () => Math.ceil(num_charities/CHARITIES_PER_PAGE)
+
+    const getNextPages = () => {
+        //return an array of link data
+        //eg { page: int }
+        //to be passed to a mapping fn that renders nav links
+        const arr = []
+        for (let i = page + 1; i <= getNumPages(); i++) {
+            arr.push(i)
+        }
+        if (arr.length > 3) {
+            arr.splice(2,arr.length - 3)
+        }
+        return arr;
+    }
+
+    const getPrevPages = () => {
+        const arr = [];
+        for (let i = page - 1; i >= 1; i--) {
+            arr.unshift(i)
+        }
+        if (arr.length > 3) {
+            arr.splice(1, arr.length - 3)
+        }
+        return arr
+    }
+
+    const transformPages = (cur_page, i, arr) => {
+        return (
+            <>
+                {cur_page - page > 3 ? <span>...</span> : null}
+                <p className="text-xl mx-2 text-gray-500 hover:text-gray-800" onClick={()=>handlePageChange(cur_page)}>{cur_page}</p>
+                {page - cur_page > 3 ? <span>...</span> : null}
+            </>
+        )
+    }
+
     return (
         <nav className="flex flex-col pt-3">
-            <h1 className="text-lg">Found  <b>{num_charities}</b> named <i className="text-pink-400">{name}</i>...</h1>
+            <h1 className="text-lg mx-auto">Found  <b>{num_charities}</b> named <i className="text-pink-400">{name}</i>...</h1>
             <ul className="flex justify-center">
-                <button className="text-xl font-bold text-blue-400 hover:text-blue-500" onClick={() => handlePageChange(page - 1)} disabled={page <= 1}>{"< Previos"}</button>
-                <p className="text-xl mx-2 text-gray-500">{page}</p>
+                <button className="text-xl font-bold text-blue-400 hover:text-blue-500" onClick={() => handlePageChange(page - 1)} disabled={page <= 1}>{"< Previous"}</button>
+                {
+                    getPrevPages().map(transformPages)
+                }
+                <p className="text-xl underline mx-2 text-gray-500">{page}</p>
+                {
+                    getNextPages().map(transformPages)
+                }
                 <button className="text-xl font-bold text-blue-400 hover:text-blue-500" onClick={() => handlePageChange(page + 1)} disabled={!has_next_page}>{"Next >"}</button>
             </ul>
         </nav>
